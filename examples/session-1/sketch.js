7d886abe5f2116c959c013d6b0049152597f1959
alexandra-anima <apalocz@users.noreply.github.com>
2026-09-04 08:39:49 -0400

    Update sketch.js

--- examples/session-1/sketch.js
+++ examples/session-1/sketch.js
@@ -5,7 +5,17 @@ function setup() {
 function draw() {
   background(233);
 
-  line(0,0,400,400);
+  // top lines
+  line(0,0,200,400);
+  line(0,0,100,400);
+  line(0,0,50,400);
+  line(0,0,25,400);
+
+  // bottom lines
+  line(0,0,400,200);
+  line(0,0,400,100);
+  line(0,0,400,50);
+  line(0,0,400,25);
 
 
 }
